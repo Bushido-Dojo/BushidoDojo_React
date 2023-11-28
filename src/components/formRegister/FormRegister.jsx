e3e9e4032f6a863f56e5678c86697b4cf06e5576
juliosaas <juliocesardourado05@gmail.com>
2023-11-27 21:19:15 -0300

    finish register page

--- src/components/formRegister/FormRegister.jsx
+++ src/components/formRegister/FormRegister.jsx
@@ -1,60 +1,53 @@
 import React from "react";
 import styles from "./FormRegister.module.css";
+import { Link } from "react-router-dom";
 //
 const FormRegister = () => {
   return (
-    <form action="" method="" className={styles.form}>
+    <div className={styles.form}>
+      <h1 id={styles.h1}>Crie sua conta!</h1>
 
-      <div className={styles.div2}>
-
-        <div className={styles.barra_upper}>
-          <p className={styles.crie_sua_conta}>Crie sua conta!</p>
+      <div className={styles.submit}>
+        <div className={styles.parse}>
+          <div className={styles.input}>
+            <p>
+              <label>Nome:</label>
+            </p>
+            <input type="nome" />
+          </div>
+          <div className={styles.input}>
+            <p>
+              <label>Sobrenome:</label>
+            </p>
+            <input type="sobrenome" />
+          </div>
         </div>
-
         <div className={styles.input}>
-          <p><label>Nome:</label></p>
-          <input type="nome" />
+          <label>CPF:</label>
+          <input type="cpf" />
         </div>
         <div className={styles.input}>
-          <p><label>Sobrenome:</label></p>
-          <input type="sobrenome" />
+          <label>Email:</label>
+          <input type="email" />
         </div>
+        <div className={styles.input}>
+          <label>Celular:</label>
+          <input type="celular" />
+        </div>
+        <div className={styles.input}>
+          <label>Data nascimento:</label>
+          <input type="data" />
+        </div>
+        <div className={styles.input}>
+          <label>Senha:</label>
+          <input type="password" />
+        </div>
+        <button type="submit">Cadastrar</button>
       </div>
-
-      <div className={styles.input}>
-        <label>CPF:</label>
-        <input type="cpf" />
-      </div>
-
-      <div className={styles.input}>
-        <label>Email:</label>
-        <input type="email" />
-      </div>
-
-      <div className={styles.input}>
-        <label>Celular:</label>
-        <input type="celular" />
-      </div>
-
-      <div className={styles.input}>
-        <label>Data nascimento:</label>
-        <input type="data" />
-      </div>
-
-      <div className={styles.input}>
-        <label>Senha:</label>
-        <input type="password" />
-      </div>
-
-      <button type="submit">Cadastrar</button>
       <div className="login">
-        <p>Já possui cadastro?</p>
+        <p>Já possui cadastro? <Link to={"/login"}>Login</Link></p>
       </div>
-      
-      <div className="login_link">
-        Fazer Login <a href="#"></a>
-      </div>
-    </form>
+    </div>
   );
 };
 
